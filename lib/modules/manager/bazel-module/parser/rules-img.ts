ba89575a74527886a8275042e4a4b4bd569d2e8c
Michael Kriese <michael.kriese@visualon.de>
2025-08-14 08:57:32 +0200

    refactor(zod): rename schema to match our best practices guid

--- lib/modules/manager/bazel-module/parser/rules-img.ts
+++ lib/modules/manager/bazel-module/parser/rules-img.ts
@@ -1,23 +1,21 @@
 import { z } from 'zod';
 import { DockerDatasource } from '../../../datasource/docker';
 import type { PackageDependency } from '../../types';
-import { RepoRuleFunctionCallSchema, StringFragmentSchema } from './fragments';
+import { RepoRuleFunctionCall, StringFragment } from './fragments';
 
 export const rulesImgLoadPaths = ['@rules_img//img:pull.bzl'] as const;
 
-export const RulesImgPullToDockerPackageDep = RepoRuleFunctionCallSchema.extend(
-  {
-    loadPath: z.enum(rulesImgLoadPaths),
-    originalFunctionName: z.literal('pull'),
-    children: z.object({
-      name: StringFragmentSchema,
-      digest: StringFragmentSchema,
-      registry: StringFragmentSchema,
-      repository: StringFragmentSchema,
-      tag: StringFragmentSchema.optional(),
-    }),
-  },
-).transform(
+export const RulesImgPullToDockerPackageDep = RepoRuleFunctionCall.extend({
+  loadPath: z.enum(rulesImgLoadPaths),
+  originalFunctionName: z.literal('pull'),
+  children: z.object({
+    name: StringFragment,
+    digest: StringFragment,
+    registry: StringFragment,
+    repository: StringFragment,
+    tag: StringFragment.optional(),
+  }),
+}).transform(
   ({
     rawString,
     functionName,
